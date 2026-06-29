da43d2ee22cba274a84f4e2603e99e12518f0ec1
aryan13a <73010565+aryan13a@users.noreply.github.com>
2026-06-29 13:02:03 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://webio.agency",
+      url: "https://www.webio.co.in",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1.0,

ec09ad66b161db6dd6003dbed87dd899dca3e39f
aryan13a <73010565+aryan13a@users.noreply.github.com>
2026-06-29 13:02:29 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://webio.agency/sitemap.xml",
+    sitemap: "https://www.webio.co.in/sitemap.xml",
   };
 }
